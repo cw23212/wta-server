da618c555e6661b9e7a06fc2a34b4d0dcd361099
psj <psj2867@gmail.com>
2024-06-01 22:10:16 +0900

    cors 설정 변경

--- core/config/app_config.py
+++ core/config/app_config.py
@@ -25,7 +25,9 @@ def app(debug:bool = False)->FastAPI:
     app.add_middleware(
         CORSMiddleware,
         allow_origins=origins,
+        allow_credentials=True,
         allow_methods=["*"],
         allow_headers=["*"],
+        expose_headers=["*"]
     )
     return app
